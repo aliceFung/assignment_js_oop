d8de9d27a8b579d4417e8fa9a995b447bc225402
Alice Fung <alice.o.fung@gmail.com>
2015-08-31 14:05:08 -0400

    asteroid

--- asteroids.js
+++ asteroids.js
@@ -1,21 +1,57 @@
+model = {
+
+
+
+  Asteroid: function(x, y, radius){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.xSpeed = 2;
+    this.ySpeed = 2;
+    console.log("this is: " + this);
+
+  }
+
+};
+
+
+model.Asteroid.prototype.tic = function(){
+        console.log("this is: " + this);
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+        console.log("tic fn: " +this.x +", " + this.y);
+    };
+
+
+
+
 view = {
 
   context: $("#board")[0].getContext("2d"),
 
-  drawRect: function(){
-    view.context.fillStyle = "#ABCDEF"
-    view.context.fillRect(5,5, 90, 90)
-    console.log("drew rect")
+  // drawRect: function(){
+  //   view.context.fillStyle = "#ABCDEF"
+  //   view.context.fillRect(5,5, 90, 90)
+  //   console.log("drew rect")
+  // },
+
+  drawCircle: function(x,y,radius){
+    var path = new Path2D();
+    path.arc(x,y,radius,0,Math.PI*2,false);
+    view.context.stroke(path);
   },
 
-  drawCircle: function(){
-    var path = new Path2D();
-    path.arc(600,400,50,0,Math.PI,false);
-    view.context.stroke(path);
+  //~ animation, draw one, clear, draw
+  fillCanvas: function(){
+    view.context.fillStyle = '#ffffff';
+    view.context.fillRect(0,0, 1200, 800); //overwriting canvas
   }
 
-}
+};
 
 
 
-view.drawCircle()
+view.drawCircle(600,400, 50);
+view.fillCanvas()
+view.drawCircle(601, 401, 50);
+
